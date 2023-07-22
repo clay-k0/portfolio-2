dd0df0a01a334a8d83f1309aeaab84ca123eaf70
Clayton King <clayk@M1-MacBook-Pro.fios-router.home>
2023-07-22 04:24:45 -0400

    lots of styling updates

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         quantity={100}
       /> */}
       <h1 className='z-10 pb-4 text-5xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text '>
-        clayco
+        clay-k0
       </h1>
       <div className='hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
       <div className='my-14 text-center animate-fade-in'>
